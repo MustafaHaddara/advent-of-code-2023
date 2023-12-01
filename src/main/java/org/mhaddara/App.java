87ed81417e073dd1dcf3b571bee7eee48d4602e0
Mustafa Haddara <mustafaayhaddara@yahoo.com>
2023-11-30 22:01:08 -0500

    add execution harness

--- src/main/java/org/mhaddara/App.java
+++ src/main/java/org/mhaddara/App.java
@@ -1,13 +1,39 @@
 package org.mhaddara;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+import org.mhaddara.solutions.*;
+
+public class App {
+    static List<Solution> solutions = List.of();
+
+    public static void main( String[] args ) throws IOException {
+        // args: day, part, test or real
+        int dayNum = Integer.parseInt(args[0]);
+        String part = args[1];
+        boolean testMode = Boolean.parseBoolean(args[2]);
+
+        Solution solution = solutions.get(dayNum - 1);
+
+        List<String> lines = testMode ? solution.testInput().lines().toList() : getInput(dayNum);
+
+        System.out.println(lines);
+        
+        String result = switch (part) {
+            case "A" -> solution.solvePartOne(lines);
+            case "B" -> solution.solvePartTwo(lines);
+            default -> "Invalid part";
+        };
+
+        System.out.println(result);
+    }
+
+    private static List<String> getInput(int day) throws IOException {
+        String fileName = String.format("src/resources/inputs/day%02d.txt", day);
+        List<String> lines = Files.readAllLines(Path.of(fileName));
+        return lines;
     }
 }
---
+++ src/main/java/org/mhaddara/Solution.java
@@ -0,0 +1,9 @@
+package org.mhaddara;
+
+import java.util.List;
+
+public interface Solution {
+    public String testInput();
+    public String solvePartOne(List<String> input);
+    public String solvePartTwo(List<String> input);
+}
